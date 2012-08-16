84cdbe079ab7d46162e6e03dc8d84e0006f1f057
Martyn Taylor <mtaylor@redhat.com>
2012-08-16 18:36:33 +0100

    updated comments on model

--- lib/accommodation_matcher/price_range.rb
+++ lib/accommodation_matcher/price_range.rb
@@ -1,4 +1,4 @@
-# This class is 100% neccessary we could just total and free as attributes
+# This class is not 100% neccessary we could just total and free as attributes
 # to the Accommodation Class.  This approach was chosen to simplying processing
 # of the JSON and in to simply converting back into JSON if needed in future
 module AccommodationMatcher
